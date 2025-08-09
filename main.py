491a3eb224af74b39281e4d1e19c21e105d1ac06
Sakshyam <sakshyamshrestha8@gmail.com>
2025-08-09 20:41:15 +0545

    Add places for add and remove task func

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 def main():
+    separate()
     print("Ready to do some tasks!")
     open_list()
-    
+    separate()
+    while True:
+        print("What do you want to do:")
+        print("1. Edit       2. Exit")
+        usr_choice = str(input("->").lower())
+        separate()
+        if usr_choice in ("edit","1"):
+            print("You wanna: 1. Add  2.Remove")
+            edit_choice = str(input("->")).lower()
+            separate()
+            if edit_choice == "add" or "1":
+                print("Add")
+                separate()
+            else:
+                print("Remove")
+                separate()
+        elif usr_choice in ("exit","2"):
+            return False
+        
     
 def open_list():
     with open("tasks.txt", "r") as file:
@@ -9,6 +28,11 @@ def open_list():
         tasks = [content.strip().capitalize() for content in contents]
         for i, task in enumerate(tasks):
             print(f"{i+1}. {task}")
+            
+            
+def separate():
+    separator = "================================="
+    print(separator)
         
-        
+
 main()
